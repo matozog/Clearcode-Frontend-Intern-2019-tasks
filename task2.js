69b682fd0a9894beab312474b23b7f3f0f1896c9
matozog <226125@student.pwr.edu.pl>
2019-06-09 23:21:22 +0200

    create solution and JSDoc to task2

--- task2.js
+++ task2.js
@@ -1,84 +1,173 @@
-const board = [
-    [ 7, 7, 7, 5, 1, 7 ],
-    [ 1, 8, 1, 1, 1, 4 ],
-    [ 3, 2, 3, 9, 7, 6 ],
-    [ 9, 9, 3, 3, 6, 2 ],
-    [ 1, 9, 3, 1, 8, 7 ],
-    [ 5, 9, 2, 2, 4, 8 ]
-];
-
-// [ 0, 0, 0, 0, 0, 7 ],
-// [ 1, 8, 0, 5, 1, 4 ],
-// [ 3, 2, 3, 9, 7, 6 ],
-// [ 9, 9, 3, 3, 6, 2 ],
-// [ 1, 9, 3, 1, 8, 7 ],
-// [ 5, 9, 2, 2, 4, 8 ]
-
-// [ 0, 0, 0, 0, 0, 7 ],
-// [ 1, 0, 0, 5, 1, 4 ],
-// [ 3, 0, 0, 9, 7, 6 ],
-// [ 9, 0, 0, 3, 6, 2 ],
-// [ 1, 8, 0, 1, 8, 7 ],
-// [ 5, 2, 2, 2, 4, 8 ]
-
-// [ 0, 0, 0, 0, 0, 7 ],
-// [ 1, 0, 0, 0, 1, 4 ],
-// [ 3, 0, 0, 5, 7, 6 ],
-// [ 9, 0, 0, 9, 6, 2 ],
-// [ 1, 0, 0, 3, 8, 7 ],
-// [ 5, 8, 0, 1, 4, 8 ]
-
+/**
+ * Main function, if board dimensions are right it run function checkBoard which solve riddle.
+ * @param {Array.<integer[]>} board 
+ * @returns {Array.<integer[]> | string}
+ * @see checkBoard
+ */
 function riddleSolver(board){
-    board.map((row, rowIndex, board) => {
-        row.map((number, numberIndex) => {
-            if(number!==0){
-                if(numberIndex<row.length-2)
-                    checkHorizontally(board, row, numberIndex, number, rowIndex)
-                if(rowIndex<board.length-2)
-                    checkVertically(board, row, board[rowIndex+1], board[rowIndex+2], numberIndex, number, rowIndex )
+    if(boardIsInRange(board)){
+        while(checkBoard(board)){}
+        return board
+    }else{
+        return "The board is out of size"
+    }
+}
+
+/**
+ * Checks whether board has the right size, return verification result.
+ * @param {Array.<integer[]>} board 
+ * @returns {boolean}
+ */
+const boardIsInRange = board => {
+    return (board.length >= 3 && board.length <=50 && board[0].length>=3 && board[0].length<=50)?true:false
+}
+
+/**
+ * Iterate two dimensional array of integers (from the upper left corner to lower right) and check if two elements are on the right horizontally
+ * or they are down vertically from the currently checked integer. If it is true, execute functions that remove integers (assing value 0)
+ * and drop elements above them.
+ * @example
+ * var board = [[1,2,3], [1, 1, 1], [3,4,5]]
+ * console.log(checkBoard(bard)) // [[0, 0, 0], [1,2,3], [3,4,5]]
+ * @param {Array.<integer[]>} board 
+ * @returns {boolean} If board was modified return true otherwise false. It helps to finish checking.
+ */
+function checkBoard(board){
+    let isChanged = false
+    board.map((row, rowIndex) => {
+        row.map((integer, integerIndex) => {
+            if(isPossibleHorizontalyReduction(row, integerIndex)){
+                if(checkAndRemoveHorizontally(board, rowIndex, integerIndex)){
+                    (rowIndex > 0) && dropIntegersHorizontally(board, rowIndex, integerIndex)
+                    isChanged = true
+                }
+            }
+            if(isPossibleVerticallyReduction(rowIndex, board.length, integer)){
+                if(checkAndRemoveVertically(board, rowIndex, integerIndex)){
+                    if(rowIndex!==0 && board[rowIndex-1][integerIndex]!==0)
+                        dropIntegersVertically(board, integerIndex, rowIndex)
+                    isChanged = true
+                }
             }
         })
     })
-    console.log(board)
+    return isChanged;
 }
 
-function checkHorizontally(board, row, numberIndex, number, rowIndex){
-    if(row[numberIndex+1]===number && row[numberIndex+2]===number){
-        row[numberIndex] = 0
-        row[numberIndex+1] = 0
-        row[numberIndex+2] = 0
-        if(rowIndex>0)
-            dropIntegersHorizontally(board, numberIndex, rowIndex)
-    }
+/**
+ * Checks possibility to horizontally reducing elements. 
+ * @param {Array.<integer>} row 
+ * @param {integer} integerIndex 
+ * @returns {boolean} If reduction is possible return true otherwise false.
+ */
+const isPossibleHorizontalyReduction = (row, integerIndex) =>{
+    return (row[integerIndex] !== 0 && integerIndex<row.length-2)?true:false
 }
 
-function dropIntegersHorizontally(board, numberIndex, rowIndex){
-    let tmpRowIndex = rowIndex
-    while(tmpRowIndex-1 >= 0){
-        for(let i=0; i<3; i=i+1){
-            swap(board, tmpRowIndex, numberIndex+i)
+/**
+ * Checks possibility to vertically reducing elements. 
+ * @param {integer} rowIndex 
+ * @param {integer} boardLength 
+ * @param {integer} integer 
+ * @returns {boolean} If reduction is possible return true otherwise false.
+ */ 
+const isPossibleVerticallyReduction = (rowIndex, boardLength, integer) => {
+    return (integer !== 0 && rowIndex<boardLength-2)?true:false
+}
+
+/**
+ * If two elements next to actual integer are the same it reduces they.
+ * @param {Array.<integer[]>} board 
+ * @param {integer} rowIndex 
+ * @param {integer} integerIndex 
+ * @returns {boolean} If reduced return true otherwise false.
+ * @see horizontallyCondition
+ */
+function checkAndRemoveHorizontally(board, rowIndex, integerIndex){
+    if(horizontallyCondition(board, rowIndex, integerIndex)){
+        for(i=0; i<3; i++){
+            board[rowIndex][integerIndex+i] = 0
         }
-        tmpRowIndex=tmpRowIndex-1
+        return true
     }
+    return false
 }
 
-function swap(board, rowIndex, numberIndex){
-    let tmp = board[rowIndex][numberIndex]
-    board[rowIndex][numberIndex] = board[rowIndex-1][numberIndex]
-    board[rowIndex-1][numberIndex]=tmp
+/**
+ * Checks horizontal condition for reduction (two elements on the right next to checked).
+ * @param {Array.<integer[]>} board 
+ * @param {integer} rowIndex 
+ * @param {integer} integerIndex 
+ * @returns {boolean} If condition elements are equals return true otherwise false.
+ */
+const horizontallyCondition = (board, rowIndex, integerIndex) =>{
+    let checkedInteger = board[rowIndex][integerIndex]
+    return (board[rowIndex][integerIndex+1]===checkedInteger && board[rowIndex][integerIndex+2]===checkedInteger)?true:false
 }
 
-function checkVertically(board, firstRow, secondRow, thirdRow, numberIndex, number, rowIndex){
-    if(secondRow[numberIndex] === number && thirdRow[numberIndex] == number){
-        firstRow[numberIndex] = 0
-        secondRow[numberIndex] = 0
-        thirdRow[numberIndex] = 0
-        if(board[rowIndex-1][numberIndex]!=0){
-            dropIntegersVertically(board, numberIndex, rowIndex)
+/**
+ * Drops elements (from above) for three column in which we have reduced the elements until the row index is greater than or equal to 0.
+ * @param {Array.<integer[]>} board 
+ * @param {integer} rowIndex 
+ * @param {integer} integerIndex 
+ */
+function dropIntegersHorizontally(board,rowIndex, integerIndex){
+    while(rowIndex-1 >= 0){
+        for(i=0; i<3; i=i+1){
+            swap(board, rowIndex, integerIndex+i)
         }
+        rowIndex=rowIndex-1
     }
 }
 
+/**
+ * Swap two elements (current and one above it) in two-dimensional array.
+ * @param {Array.<integer[]>} board 
+ * @param {integer} rowIndex 
+ * @param {integer} integerIndex 
+ */
+const swap = (board, rowIndex, integerIndex) =>{
+    let tmp = board[rowIndex][integerIndex]
+    board[rowIndex][integerIndex] = board[rowIndex-1][integerIndex]
+    board[rowIndex-1][integerIndex]=tmp
+}
+
+/**
+ * If two elements below actual integer are the same (verticallyCondition) it reduces they.
+ * @param {Array.<integer[]>} board 
+ * @param {integerIndex} rowIndex 
+ * @param {integer} integerIndex 
+ * @returns {boolean} If reduced return true otherwise false.
+ * @see verticallyCondition
+ */
+function checkAndRemoveVertically(board, rowIndex, integerIndex){
+    if(verticallyCondition(board, rowIndex, integerIndex)){
+        for(i=0; i<3; i++){
+            board[rowIndex+i][integerIndex] = 0
+        }
+        return true
+    }
+    return false
+}
+
+/**
+ * Checks vertical condition for reduction (two elements below to checked).
+ * @param {Array.<integer[]>} board 
+ * @param {integer} rowIndex 
+ * @param {integer} integerIndex 
+ * @returns {boolean} If condition elements are equals return true otherwise false.
+ */
+const verticallyCondition = (board, rowIndex, integerIndex) => {
+    let checkedInteger = board[rowIndex][integerIndex]
+    return (board[rowIndex+1][integerIndex] === checkedInteger && board[rowIndex+2][integerIndex] == checkedInteger)?true:false
+}
+
+/**
+ * Drops elements for one column in which we have reduced the elements until the element above is 0 or the row index is equal to 0.
+ * @param {Array.<integer[]>} board 
+ * @param {integer} numberIndex 
+ * @param {integer} rowIndex 
+ */
 function dropIntegersVertically(board, numberIndex, rowIndex){
     let tmpRowIndex = rowIndex
     while(tmpRowIndex-1 >=0 && board[tmpRowIndex-1][numberIndex] !== 0){
@@ -91,4 +180,6 @@ function dropIntegersVertically(board, numberIndex, rowIndex){
     }
 }
 
-console.log(riddleSolver(board))
+module.exports = {
+    riddleSolver: riddleSolver
+}
